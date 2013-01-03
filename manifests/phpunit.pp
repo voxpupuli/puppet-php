083e0282cbad4cf45c54e4786db80c67e4e2757b
Christian Winther <cw@nodes.dk>
2013-01-03 21:45:36 +0100

    Add phpunit

---
+++ manifests/phpunit.pp
@@ -0,0 +1,19 @@
+class php::phpunit {
+
+	package {
+		"pear.phpunit.de/PHPUnit":
+			ensure => "3.7.10",
+			provider => "pear";
+	}
+
+	exec {
+		"pear auto_discover":
+			command => "pear config-set auto_discover 1 system",
+			refreshonly => true;
+	}
+
+	Package["php-pear"]
+		-> Exec["pear auto_discover"]
+		-> Package["pear.phpunit.de/PHPUnit"]
+
+}
