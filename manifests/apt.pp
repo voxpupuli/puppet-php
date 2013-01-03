d1b6b7b571d8e656c1cf54c608bb8be4df549573
Christian Winther <cw@nodes.dk>
2013-01-03 12:26:04 +0100

    Move gpg files from /tmp to /usr/local
    This avoid a re-run on every reboot since /tmp is automatically cleaned on boot

--- manifests/apt.pp
+++ manifests/apt.pp
@@ -10,8 +10,8 @@ class php::apt {
 
 	exec {
 		"add_dotdeb_key":
-			command 	=> 'curl --silent "http://www.dotdeb.org/dotdeb.gpg" > /tmp/dotdeb.gpg && cat /tmp/dotdeb.gpg | sudo apt-key add -',
-			creates		=> "/tmp/dotdeb.gpg";
+			command 	=> 'curl --silent "http://www.dotdeb.org/dotdeb.gpg" > /tmp/dotdeb.gpg && cat /tmp/dotdeb.gpg | sudo apt-key add - && touch /var/local/dotdeb.gpg.done',
+			creates		=> "/var/local/dotdeb.gpg.done";
 	}
 
 	Exec["add_dotdeb_key"] -> Apt::Source["dotdeb"] -> Exec["apt_update"]
