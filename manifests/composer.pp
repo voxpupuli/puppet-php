7bcb072df0717960d8891f83db1f493206da815b
Christian Winther <cw@nodes.dk>
2013-01-25 20:06:27 +0100

    Simply composer

--- manifests/composer.pp
+++ manifests/composer.pp
@@ -24,6 +24,15 @@
 #
 class php::composer {
 
-  include php::composer::package
+  exec { 'download composer':
+    command => 'wget http://getcomposer.org/composer.phar -O /usr/local/bin/composer',
+    creates => '/usr/local/bin/composer'
+  }
 
+  file { '/usr/local/bin/composer':
+    mode    => '0755',
+    owner   => root,
+    group   => root,
+    require => Exec['download composer']
+  }
 }
