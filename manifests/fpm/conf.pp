2a3182912f1496e428f1fba97f67deea6a3794cb
Christian Winther <cw@bownty.com>
2013-04-29 10:42:08 +0200

    add fpm base dirs

---
+++ manifests/fpm/conf.pp
@@ -0,0 +1,80 @@
+# Define: php::fpm::conf
+#
+# PHP FPM pool configuration definition. Note that the original php-fpm package
+# includes a pre-configured one called 'www' so you should either use that name
+# in order to override it, or "ensure => absent" it.
+#
+# Sample Usage:
+#  php::fpm::conf { 'www': ensure => absent }
+#  php::fpm::conf { 'customer1':
+#      listen => '127.0.0.1:9001',
+#      user   => 'customer1',
+#  }
+#  php::fpm::conf { 'customer2':
+#      listen => '127.0.0.1:9002',
+#      user   => 'customer2',
+#  }
+#
+define php::fpm::conf (
+  $ensure = 'present',
+  $listen = '127.0.0.1:9000',
+  # Puppet does not allow dots in variable names
+  $listen_backlog = '-1',
+  $listen_allowed_clients = '127.0.0.1',
+  $listen_owner = undef,
+  $listen_group = undef,
+  $listen_mode = undef,
+  $user = 'apache',
+  $group = undef,
+  $pm = 'dynamic',
+  $pm_max_children = '50',
+  $pm_start_servers = '5',
+  $pm_min_spare_servers = '5',
+  $pm_max_spare_servers = '35',
+  $pm_max_requests = '0',
+  $pm_status_path = undef,
+  $ping_path = undef,
+  $ping_response = 'pong',
+  $request_terminate_timeout = '0',
+  $request_slowlog_timeout = '0',
+  $slowlog = "/var/log/php-fpm/${name}-slow.log",
+  $rlimit_files = undef,
+  $rlimit_core = undef,
+  $chroot = undef,
+  $chdir = undef,
+  $catch_workers_output = 'no',
+  $env = [],
+  $env_value = {},
+  $php_value = {},
+  $php_flag = {},
+  $php_admin_value = {},
+  $php_admin_flag = {},
+  $php_directives = [],
+  $error_log = true
+) {
+
+  $pool = $title
+
+  # Hack-ish to default to user for group too
+  $group_final = $group ? { undef => $user, default => $group }
+
+  if ( $ensure == 'absent' ) {
+
+    file { "/etc/php-fpm.d/${pool}.conf":
+      notify => Service['php-fpm'],
+      ensure => absent,
+    }
+
+  } else {
+
+    file { "/etc/php-fpm.d/${pool}.conf":
+      notify  => Service['php-fpm'],
+      content => template('php/fpm/pool.conf.erb'),
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+    }
+
+  }
+
+}
---
+++ manifests/fpm/daemon.pp
@@ -0,0 +1,59 @@
+# Class: php::fpm::daemon
+#
+# Install the PHP FPM daemon. See php::fpm::conf for configuring its pools.
+#
+# Sample Usage:
+#  include php::fpm::daemon
+#
+class php::fpm::daemon (
+  $ensure = 'present',
+  $log_level = 'notice',
+  $emergency_restart_threshold = '0',
+  $emergency_restart_interval = '0',
+  $process_control_timeout = '0',
+  $log_owner = 'root',
+  $log_group = false,
+  $log_dir_mode = '0770'
+) {
+
+  # Hack-ish to default to user for group too
+  $log_group_final = $log_group ? {
+    false   => $log_owner,
+    default => $log_group,
+  }
+
+  if ( $ensure == 'absent' ) {
+
+    package { 'php5-fpm': ensure => absent }
+
+  } else {
+
+    package { 'php5-fpm': ensure => installed }
+
+    service { 'php5-fpm':
+      ensure    => running,
+      enable    => true,
+      restart   => '/sbin/service php-fpm reload',
+      hasstatus => true,
+      require   => Package['php-fpm'],
+    }
+
+    # When running FastCGI, we don't always use the same user
+    file { '/var/log/php-fpm':
+      owner   => $log_owner,
+      group   => $log_group_final,
+      mode    => $log_dir_mode,
+      require => Package['php5-fpm'],
+    }
+
+    file { '/etc/php-fpm.conf':
+      notify  => Service['php-fpm'],
+      content => template('php/fpm/php-fpm.conf.erb'),
+      owner   => 'root',
+      group   => 'root',
+      mode    => 0644,
+    }
+
+  }
+
+}
