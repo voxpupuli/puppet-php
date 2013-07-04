af0785db087f16c8bb43fb7315c0f630360d225d
Christian Winther <cw@bownty.com>
2013-07-04 12:07:56 +0200

    no sudo

--- manifests/apt.pp
+++ manifests/apt.pp
@@ -39,7 +39,7 @@ class php::apt(
 
   if ($dotdeb) {
     exec { 'add_dotdeb_key':
-      command => 'curl --silent "http://www.dotdeb.org/dotdeb.gpg" > /tmp/dotdeb.gpg && cat /tmp/dotdeb.gpg | sudo apt-key add - && touch /var/local/dotdeb.gpg.done',
+      command => 'curl --silent "http://www.dotdeb.org/dotdeb.gpg" > /tmp/dotdeb.gpg && cat /tmp/dotdeb.gpg | apt-key add - && touch /var/local/dotdeb.gpg.done',
       creates => '/var/local/dotdeb.gpg.done',
       path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ];
     }
