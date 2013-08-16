b8cc4d0c0676e0a22aa1498d63e0c85710476dcb
Goran Miskovic <goran.miskovic@arbor-education.com>
2013-08-16 15:43:59 +0200

    Correcting: Evaluating manifests/cli.pp ERROR: tab character found on line 50 ERROR: tab character found on line 51 ERROR: tab character found on line 55 WARNING: => on line isn't properly aligned for resource on line 56

--- manifests/cli.pp
+++ manifests/cli.pp
@@ -47,12 +47,12 @@ class php::cli(
 ) inherits php::cli::params {
 
   php::contrib::base_package { 'cli':
-  	ensure   => $ensure,
-  	provider => $provider;
+    ensure   => $ensure,
+    provider => $provider;
   }
 
   package { $package:
-    ensure	 => $ensure,
+    ensure   => $ensure,
     provider => $provider;
   }
 
