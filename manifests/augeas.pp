6db9360847abcc89cf81348af89dd0c2c23cfeaf
Goran Miskovic <goran.miskovic@arbor-education.com>
2013-08-20 10:56:11 +0200

    manifests/augeas.pp - WARNING: double quoted string containing no variables on line 32 manifests/augeas.pp - WARNING: double quoted string containing no variables on line 38 manifests/augeas.pp - WARNING: double quoted string containing no variables on line 39

--- manifests/augeas.pp
+++ manifests/augeas.pp
@@ -29,14 +29,14 @@
 class php::augeas {
 
   if !defined(File['/usr/share/augeas/lenses/contrib']) {
-    file { "/usr/share/augeas/lenses/contrib":
+    file { '/usr/share/augeas/lenses/contrib':
       ensure  => directory,
       recurse => true,
       purge   => true,
       force   => true,
       mode    => '0644',
-      owner   => "root",
-      group   => "root"
+      owner   => 'root',
+      group   => 'root'
     }
   }
 
