d2db45b6163819fda95a6707c06cb00cd2a98893
Arthur Leonard Andersen <leoc.git@gmail.com>
2013-09-22 00:53:21 +0200

    Add ldap extension

---
+++ manifests/extension/ldap/params.pp
@@ -0,0 +1,50 @@
+# == Class: php::extension::ldap::params
+#
+# Defaults file for the ldap PHP extension
+#
+# === Parameters
+#
+# No parameters
+#
+# === Variables
+#
+# [*ensure*]
+#   The version of the package to install
+#   Could be "latest", "installed" or a pinned version
+#   This matches "ensure" from Package
+#
+# [*package*]
+#   The package name in your provider
+#
+# [*provider*]
+#   The provider used to install the package
+#
+# [*inifile*]
+#   The path to the extension ini file
+#
+# [*settings*]
+#   Hash with 'set' nested hash of key => value
+#   set changes to agues when applied to *inifile*
+#
+# === Examples
+#
+# No examples
+#
+# === Authors
+#
+# Arthur Andersen <leoc.git@gmail.com>
+#
+# === Copyright
+#
+# Copyright 2012-2013 Nodes, unless otherwise noted.
+#
+class php::extension::ldap::params {
+
+  $ensure   = $php::params::ensure
+  $package  = 'php5-ldap'
+  $provider = undef
+  $inifile  = '/etc/php5/conf.d/20-ldap.ini'
+  $settings = {
+    set => {}
+  }
+}
