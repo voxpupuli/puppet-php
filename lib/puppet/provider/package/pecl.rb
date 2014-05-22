93172d2e1990cf2cf109cf22253ea2bb7b7caaf8
sw0x2A <sw@00101010.org>
2014-05-22 15:44:51 +0200

    Match entire pecl package name, not just beginning
    Conflicts:
    	lib/puppet/provider/package/pecl.rb

--- lib/puppet/provider/package/pecl.rb
+++ lib/puppet/provider/package/pecl.rb
@@ -20,7 +20,7 @@ Puppet::Type.type(:package).provide :pecl, :parent => Puppet::Provider::Package 
     begin
       list = execute(command).split("\n").collect do |set|
         if hash[:justme]
-          if /^#{hash[:justme]}/i.match(set)
+          if /^#{hash[:justme]}$/i.match(set)
             if peclhash = peclsplit(set)
               peclhash[:provider] = :peclcmd
               peclhash
