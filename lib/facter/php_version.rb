b69679b20143ef555df1d58879a36cd93a730dfc
Andreas Ntaflos <daff@pseudoterminal.org>
2014-05-26 16:13:56 +0200

    Silence php_version fact if no PHP is installed
    Redirect stderr to /dev/null, thereby keeping the Puppet agent run quiet on nodes where no PHP is installed.

--- lib/facter/php_version.rb
+++ lib/facter/php_version.rb
@@ -1,7 +1,7 @@
 Facter.add(:php_version) do
   confine :kernel => :linux
   setcode do
-    version = Facter::Util::Resolution.exec("/usr/bin/env php -r 'echo PHP_VERSION;'")
+    version = Facter::Util::Resolution.exec("/usr/bin/env php -r 'echo PHP_VERSION;' 2>/dev/null")
     if version
       version.match(/(\d+\.\d+\.\d+)/).to_s
     else
