8d73bb5f20da7d52039b1bdb1ff8cc8ef9eb1a7b
Franz Pletz <fpletz@fnordicwalking.de>
2014-07-30 19:12:08 +0200

    Refactor php::fpm

--- manifests/fpm.pp
+++ manifests/fpm.pp
@@ -16,6 +16,9 @@
 # [*group*]
 #   group to run default FPM pool as
 #
+# [*pool_base_dir*]
+#   The directory where to place FPM pool configs
+#
 # [*pools*]
 #   Hash of php::fpm::pool resources that will be created. Defaults
 #   to a single php::fpm::pool named www with default parameters.
@@ -31,35 +34,25 @@
 # === Authors
 #
 # Robin Gloster <robin.gloster@mayflower.de>
+# Franz Pletz <franz.pletz@mayflower.de>
 #
 # === Copyright
 #
 # See LICENSE file
 #
 class php::fpm(
-  $inifile  = $php::params::fpm_inifile,
-  $user     = $php::params::fpm_user,
-  $group    = undef,
-  $settings = {},
-  $pools    = { 'www' => {} },
-) inherits php::params {
+  $pools = { 'www' => {} },
+) {
 
   if $caller_module_name != $module_name {
     warning("${name} is not part of the public API of the ${module_name} module and should not be directly included in the manifest.")
   }
 
-  validate_string($inifile)
-  validate_string($user)
-  validate_string($group)
-  validate_hash($settings)
   validate_hash($pools)
 
   anchor { 'php::fpm::begin': } ->
     class { 'php::fpm::package': } ->
-    php::fpm::config { 'php-fpm':
-      file    => $inifile,
-      config  => $settings
-    } ->
+    class { 'php::fpm::config': }  ->
     class { 'php::fpm::service': } ->
   anchor { 'php::fpm::end': }
 
