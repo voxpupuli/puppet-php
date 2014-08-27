99f3dae6d6005ca12864b1bc45e1e4231990a77e
Robin Gloster <robin@loc-com.de>
2014-08-28 00:10:15 +0200

    add apache vhost

---
+++ manifests/apache_vhost.pp
@@ -0,0 +1,46 @@
+# == Class: php::apache
+#
+# Configures an apache vhost for php
+#
+# === Parameters
+#
+# [*vhost*]
+#   The vhost address
+#
+# [*docroot*]
+#   The vhost docroot
+#
+# [*port*]
+#   The vhost port
+#
+# [*default_vhost*]
+#   defines if vhost is the default vhost
+#
+# [*fastcgi_socket*]
+#   address of the fastcgi socket
+#
+# === Authors
+#
+# Christian "Jippi" Winther <jippignu@gmail.com>
+# Robin Gloster <robin.gloster@mayflower.de>
+#
+# === Copyright
+#
+# See LICENSE file
+#
+define php::apache_vhost(
+  $vhost          = 'example.com',
+  $docroot        = '/var/www',
+  $port           = 80,
+  $default_vhost  = true,
+  $fastcgi_socket = 'fcgi://127.0.0.1:9000/$1'
+) {
+
+  apache::vhost { $vhost:
+    docroot         => $docroot,
+    default_vhost   => $default_vhost,
+    port            => $port,
+    override        => 'all',
+    custom_fragment => "ProxyPassMatch ^/(.*\.php(/.*)?)$ ${fastcgi_socket}",
+  }
+}
