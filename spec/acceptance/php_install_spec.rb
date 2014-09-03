d175fa15d3f4dbab5d6641b4610709ac17bc0714
Robin Gloster <robin@loc-com.de>
2014-09-04 00:38:12 +0200

    basic test, fails because of /etc/puppetlabs/puppet vs. /etc/puppet

--- spec/acceptance/php_install_spec.rb
+++ spec/acceptance/php_install_spec.rb
@@ -1,25 +1,14 @@
 require 'spec_helper_acceptance'
 
-describe 'mysql::server::account_security class' do
+describe 'php class' do
   describe 'running puppet code' do
     it 'should work with no errors' do
       pp = <<-EOS
-        class { 'mysql::server': remove_default_accounts => true }
+        class { 'php': }
       EOS
 
-      # Run it twice and test for idempotency
       apply_manifest(pp, :catch_failures => true)
-      expect(apply_manifest(pp, :catch_failures => true).exit_code).to be_zero
-    end
-
-    describe 'accounts' do
-      it 'should delete accounts' do
-        shell("mysql -e 'show grants for root@127.0.0.1;'", :acceptable_exit_codes => 1)
-      end
-
-      it 'should delete databases' do
-        shell("mysql -e 'show databases;' |grep test", :acceptable_exit_codes => 1)
-      end
+      apply_manifest(pp, :catch_changes => true)
     end
   end
 end
--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -3,8 +3,8 @@ require 'pry'
 
 hosts.each do |host|
   # Install Puppet
-  install_package host, 'rubygems'
-  on host, 'gem install puppet --no-ri --no-rdoc'
+  #install_package host, 'rubygems'
+  #on host, 'gem install puppet --no-ri --no-rdoc'
   on host, "mkdir -p #{host['distmoduledir']}"
 end
 
@@ -18,7 +18,7 @@ RSpec.configure do |c|
   # Configure all nodes in nodeset
   c.before :suite do
     # Install module
-    puppet_module_install(:source => proj_root, :module_name => 'mysql')
+    puppet_module_install(:source => proj_root, :module_name => 'php')
     hosts.each do |host|
       on host, puppet('module','install','puppetlabs-stdlib'), { :acceptable_exit_codes => [0,1] }
     end
