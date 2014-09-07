8b142d833a5b4415a775a7b4d1cc6ccce61ae733
Robin Gloster <robin@loc-com.de>
2014-09-08 00:31:59 +0200

    fix tests for OS!=SLES

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,8 +1,9 @@
 require 'beaker-rspec'
 require 'pry'
+require 'beaker-rspec/helpers/serverspec'
 
 hosts.each do |host|
-   on host, "mkdir -p #{host['distmoduledir']}"
+  on host, "mkdir -p #{host['distmoduledir']}"
 end
 
 RSpec.configure do |c|
@@ -14,9 +15,8 @@ RSpec.configure do |c|
 
   # Configure all nodes in nodeset
   c.before :suite do
-
     hosts.each do |host|
-      on host, 'test -x /usr/bin/zypper' do |result|
+      on host, 'test -x /usr/bin/zypper', :acceptable_exit_codes => [0,1] do |result|
         if result.exit_code == 0
           on host, 'zypper addrepo -f --no-gpgcheck http://demeter.uni-regensburg.de/SLES11SP3-x64/DVD1/ "SLES11SP3-x64 DVD1 Online"'
           on host, 'zypper addrepo -f --no-gpgcheck http://demeter.uni-regensburg.de/SLE11SP3-SDK-x64/DVD1/ "SUSE-Linux-Enterprise-Software-Development-Kit-11-SP3"'
@@ -29,10 +29,10 @@ RSpec.configure do |c|
     # Install module
     puppet_module_install(:source => proj_root, :module_name => 'php')
     hosts.each do |host|
-      on host, puppet('module','install','puppetlabs-stdlib'), { :acceptable_exit_codes => [0,1] }
-      on host, puppet('module','install','puppetlabs-apt'), { :acceptable_exit_codes => [0,1] }
-      on host, puppet('module','install','puppetlabs-inifile'), { :acceptable_exit_codes => [0,1] }
-      on host, puppet('module','install','darin-zypprepo'), { :acceptable_exit_codes => [0,1] }
+      on host, puppet('module', 'install', '-f', 'puppetlabs-stdlib')
+      on host, puppet('module', 'install', '-f', 'puppetlabs-apt')
+      on host, puppet('module', 'install', '-f', 'puppetlabs-inifile')
+      on host, puppet('module', 'install', '-f', 'darin-zypprepo')
     end
   end
 end
