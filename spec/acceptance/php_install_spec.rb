7779d9cef8655914b92a72f57d392bdbdf591af6
Robin Gloster <robin@loc-com.de>
2014-09-08 00:32:24 +0200

    really test things

--- spec/acceptance/php_install_spec.rb
+++ spec/acceptance/php_install_spec.rb
@@ -4,11 +4,23 @@ describe 'php class' do
   describe 'running puppet code' do
     it 'should work with no errors' do
       pp = <<-EOS
-        class { 'php': }
+        class { 'php':
+          settings => {
+            'PHP/short_open_tag' => 'On'
+          }
+        }
       EOS
 
       apply_manifest(pp, :catch_failures => true)
       apply_manifest(pp, :catch_changes => true)
+    end
+
+    describe port(9000) do
+      it { should be_listening }
+    end
+
+    describe php_config('short_open_tag') do
+      its(:value) { should eq 1 }
     end
   end
 end
