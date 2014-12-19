28e4c34d05fc30a954492c253614a794e4c3f34e
Paul Bailey <paulba@stedwards.edu>
2014-12-18 19:18:58 -0600

    Install phpunit from the phar

--- manifests/phpunit.pp
+++ manifests/phpunit.pp
@@ -1,15 +1,20 @@
 # == Class: php::phpunit
 #
-# Install phpunit, PHP testing framework via PEAR, this will be changed to
-# use composer
+# Install phpunit, PHP testing framework
 #
 # === Parameters
 #
-# [*package*]
-#   The package name for PHPUnit, defaults to phpunit's pear package
+# [*source*]
+# Holds URL to the phpunit source file
 #
-# [*provider*]
-#   The package provider used to install PHPUnit, defaults to pear
+# [*path*]
+# Holds path to the phpunit executable
+#
+# [*auto_update*]
+# defines if phpunit should be auto updated
+#
+# [*max_age*]
+# defines the time in days after which an auto-update gets executed
 #
 # === Authors
 #
@@ -21,25 +26,39 @@
 #
 # See LICENSE file
 #
-
-#FIXME: no pear
 class php::phpunit (
-  $package  = 'pear.phpunit.de/PHPUnit',
-  $provider = 'pear',
-) {
+  $source      = $php::params::phpunit_source,
+  $path        = $php::params::phpunit_path,
+  $auto_update = true,
+  $max_age     = $php::params::phpunit_max_age,
+) inherits php::params {
 
   if $caller_module_name != $module_name {
     warning('php::phpunit is private')
   }
 
-  validate_string($package)
+  validate_string($source)
+  validate_absolute_path($path)
+  validate_bool($auto_update)
+  validate_re("${max_age}", '^\d+$')
 
-  package { $package:
-    ensure   => present,
-    provider => $provider,
+  exec { 'download phpunit':
+    command => "wget ${source} -O ${path}",
+    creates => $path,
+    path    => ['/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/'],
+    require => Class['php::cli'],
+  } ->
+  file { $path:
+    mode  => '0555',
+    owner => root,
+    group => root,
   }
 
-  if $provider == 'pear' {
-    Exec['php::pear::auto_discover'] -> Package[$package]
+  if $auto_update {
+    class { 'php::phpunit::auto_update':
+      max_age => $max_age,
+      source  => $source,
+      path    => $path
+    }
   }
 }
