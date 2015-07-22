57e2eb0414c14064e7c30d85fcf0282aca990368
Andreas de Pretis <ad@25th-floor.com>
2015-07-22 10:14:40 +0200

    Support a custom/specific PPA for Ubuntu systems
    +) added $php::repo::ubuntu::ppa
    +) conflicts with $php::repo::ubuntu::oldstable

--- spec/classes/php_repo_ubuntu_spec.rb
+++ spec/classes/php_repo_ubuntu_spec.rb
@@ -43,4 +43,35 @@ describe 'php::repo::ubuntu', :type => :class do
       should contain_exec('add-apt-repository-ppa:ondrej/php5-oldstable')
     }
   end
+
+  describe 'when call with parameter ppa without prefix "ppa:"' do
+    let(:facts) {{
+      :osfamily        => 'Debian',
+      :operatingsystem => 'Ubuntu',
+      :lsbdistid       => 'Ubuntu',
+      :lsbdistcodename => 'trusty'
+    }}
+    let(:params) {{
+      :ppa => 'ondrej/php5-5.6'
+    }}
+
+    it {
+      should contain_exec('add-apt-repository-ppa:ondrej/php5-5.6')
+    }
+  end
+
+  describe 'when call with parameter ppa and oldstable' do
+    let(:facts) {{
+      :osfamily        => 'Debian',
+      :operatingsystem => 'Ubuntu',
+      :lsbdistid       => 'Ubuntu',
+      :lsbdistcodename => 'trusty'
+    }}
+    let(:params) {{
+      :oldstable => true,
+      :ppa       => 'ondrej/php5-5.6'
+    }}
+
+    it { expect { should raise_error(Puppet::Error) }}
+  end
 end
