5a5303ddd030e6db665013d9ae2d7aff12abd252
Tim Meusel <tim@bastelfreak.de>
2016-11-02 22:40:55 +0100

    modulesync 0.15.0

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,20 @@ require 'puppetlabs_spec_helper/module_spec_helper'
 require 'rspec-puppet-facts'
 include RspecPuppetFacts
 
+unless RUBY_VERSION =~ %r{^1.9}
+  require 'coveralls'
+  require 'simplecov'
+  require 'simplecov-console'
+  SimpleCov.formatters = [
+    SimpleCov::Formatter::HTMLFormatter,
+    SimpleCov::Formatter::Console,
+    Coveralls::SimpleCov::Formatter
+  ]
+  SimpleCov.start do
+    add_filter '/spec'
+  end
+end
+
 RSpec.configure do |c|
   default_facts = {
     puppetversion: Puppet.version,
