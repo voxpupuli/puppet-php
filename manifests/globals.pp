a2982b5ca623bd990dfa5bced6cb670e09a0e9f2
Florian Strübe <f.struebe@yatego.com>
2017-01-16 23:29:40 +0100

    Add PHP 7.1 support on Debian
    use ensure_packages
    
    dont wrap apt::key in a useless create_resources

--- manifests/globals.pp
+++ manifests/globals.pp
@@ -66,7 +66,7 @@ class php::globals (
         }
       } else {
         case $globals_php_version {
-          /^7/: {
+          /^7\.[0-9]/: {
             $default_config_root  = "/etc/php/${globals_php_version}"
             $default_fpm_pid_file = "/var/run/php/php${globals_php_version}-fpm.pid"
             $fpm_error_log        = "/var/log/php${globals_php_version}-fpm.log"
