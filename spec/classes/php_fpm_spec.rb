5d8b2d79ecf64c5be0b68224aa21e966f6166e98
Rob Nelson <rnelson0@gmail.com>
2017-01-19 12:53:23 +0000

    Silence RepeatedExample rubocop

--- spec/classes/php_fpm_spec.rb
+++ spec/classes/php_fpm_spec.rb
@@ -8,6 +8,7 @@ describe 'php::fpm', type: :class do
       end
       let(:pre_condition) { 'class {"php": fpm => false}' }
       describe 'when called with no parameters' do
+        # rubocop:disable RSpec/RepeatedExample
         case facts[:osfamily]
         when 'Debian'
           it { is_expected.to contain_package('php5-fpm').with_ensure('present') }
@@ -24,6 +25,7 @@ describe 'php::fpm', type: :class do
           it { is_expected.to contain_package('php-fpm').with_ensure('present') }
           it { is_expected.to contain_service('php-fpm').with_ensure('running') }
         end
+        # rubocop:enable RSpec/RepeatedExample
         it { is_expected.to contain_class('php::fpm::config').that_notifies('Class[php::fpm::service]') }
         it { is_expected.to contain_class('php::fpm::service') }
       end
