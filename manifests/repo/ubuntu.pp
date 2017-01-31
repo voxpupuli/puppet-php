41bc5476602f8ebfb9169926939d742fd9b6c13c
Simon Rondelez <simonrondelez@me.com>
2017-01-31 13:14:44 +0100

    Add support for php 7.1

--- manifests/repo/ubuntu.pp
+++ manifests/repo/ubuntu.pp
@@ -26,7 +26,8 @@ class php::repo::ubuntu (
     '5.4' => 'ondrej/php5-oldstable',
     '5.5' => 'ondrej/php',
     '5.6' => 'ondrej/php',
-    '7.0' => 'ondrej/php'
+    '7.0' => 'ondrej/php',
+    '7.1' => 'ondrej/php'
   }
 
   if ($version != undef and $ppa != undef) {
