9bc60c4d850a936a4fdd3946a33fe5a2f60840f9
amette <mail@amette.eu>
2017-03-16 16:46:48 +0100

    Allow installing PHP 7.1 on Debian

--- manifests/globals.pp
+++ manifests/globals.pp
@@ -83,7 +83,7 @@ class php::globals (
             $fpm_service_name     = "php${globals_php_version}-fpm"
             $ext_tool_enable      = "/usr/sbin/phpenmod -v ${globals_php_version}"
             $ext_tool_query       = "/usr/sbin/phpquery -v ${globals_php_version}"
-            $package_prefix       = 'php7.0-'
+            $package_prefix       = "php${globals_php_version}-"
           }
           default: {
             $default_config_root  = '/etc/php5'
