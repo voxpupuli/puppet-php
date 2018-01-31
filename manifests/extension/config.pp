3270f9bf9497622b6087ab08721e8ca99a12ab7f
Mathew Winstone <mathew.winstone@gmail.com>
2018-01-31 17:40:10 -0500

    fix(docs) add purge docs

--- manifests/extension/config.pp
+++ manifests/extension/config.pp
@@ -4,7 +4,7 @@
 #
 # [*ensure*]
 #   The ensure of the package to install
-#   Could be "latest", "installed" or a pinned version
+#   Could be "latest", "installed" or a pinned version. "purged" or "absent" to remove.
 #
 # [*provider*]
 #   The provider used to install the package
