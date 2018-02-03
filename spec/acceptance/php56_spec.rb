6ca17a43872cc6c43948639a921817acd72adfc1
Tim Meusel <tim@bastelfreak.de>
2018-02-03 14:22:34 +0100

    add tests for php5.6

---
+++ spec/acceptance/php56_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper_acceptance'
+
+describe 'with specific php version' do
+  it 'works with 5.6' do
+    pp = <<-EOS
+    class { 'php::globals':
+      php_version => '5.6',
+    }
+    -> class { 'php':
+      ensure       => 'present',
+      manage_repos => true,
+      fpm          => true,
+      dev          => true,
+      composer     => true,
+      pear         => true,
+      phpunit      => false,
+    }
+    EOS
+    # Run it twice and test for idempotency
+    apply_manifest(pp, catch_failures: true)
+    apply_manifest(pp, catch_changes: true)
+  end
+
+  case default[:platform]
+  when %r{16.04}
+    describe package('php5.6-fpm') do
+      it { is_expected.to be_installed }
+    end
+  when %r{14.04}
+    describe package('php5.6-fpm') do
+      it { is_expected.to be_installed }
+    end
+  end
+end
