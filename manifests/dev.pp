voxpupuli/puppet-php

commit 0ad75666619af1e3b0b5723a93b5bf9acacf846d

add ubuntu 16.04 support

manifests/dev.pp
@@ -9,8 +9,9 @@
 #   The package name for the PHP development files
 #
 class php::dev(
-  String $ensure  = $::php::ensure,
-  String $package = "${::php::package_prefix}${::php::params::dev_package_suffix}",
+  String $ensure        = $::php::ensure,
+  String $package       = "${::php::package_prefix}${::php::params::dev_package_suffix}",
+  Boolean $manage_repos = $php::manage_repos,
 ) inherits ::php::params {
 
   if $caller_module_name != $module_name {
@@ -23,21 +24,31 @@ class php::dev(
     default   => $package,
   }
 
+  if $facts['os']['family'] == 'Debian' {
+    # we can set the dependency only if we manage repos
+    $require = $manage_repos ? {
+      true  => Class['::apt::update'],
+      false => undef,
+    }
+  } else {
+    $require = undef
+  }
+
   # Default PHP come with xml module and no seperate package for it
   if $facts['os']['name'] == 'Ubuntu' and versioncmp($facts['os']['release']['full'], '16.04') >= 0  {
     ensure_packages(["${php::package_prefix}xml"], {
       ensure  => present,
-      require => Class['::apt::update'],
+      require => $require,
     })
 
     package { $real_package:
       ensure  => $ensure,
-      require => Class['::php::packages'],
+      require => $require,
     }
   } else {
     package { $real_package:
       ensure  => $ensure,
-      require => Class['::php::packages'],
+      require => $require,
     }
   }
 }

manifests/pear.pp
@@ -11,6 +11,7 @@
 class php::pear (
   String $ensure            = $::php::pear_ensure,
   Optional[String] $package = undef,
+  Boolean $manage_repos     = $php::manage_repos,
 ) inherits ::php::params {
 
   if $caller_module_name != $module_name {
@@ -46,16 +47,21 @@ class php::pear (
     }
   }
 
+  # the apt module provides apt::update. apt is only included if we manage any repos
+  $require = $manage_repos ? {
+    true  => Class['::apt::update'],
+    false => undef,
+  }
   # Default PHP come with xml module and no seperate package for it
   if $facts['os']['name'] == 'Ubuntu' and versioncmp($facts['os']['release']['full'], '16.04') >= 0 {
     ensure_packages(["${php::package_prefix}xml"], {
       ensure  => present,
-      require => Class['::apt::update'],
+      require => $require,
     })
 
     package { $package_name:
       ensure  => $ensure,
-      require => [Class['::apt::update'],Class['::php::cli'],Package["${php::package_prefix}xml"]],
+      require => [$require,Class['::php::cli'],Package["${php::package_prefix}xml"]],
     }
   } else {
     package { $package_name:

spec/classes/php_fpm_service_spec.rb
@@ -13,13 +13,13 @@ describe 'php::fpm::service', type: :class do
         # rubocop:disable RSpec/RepeatedExample
         case facts[:osfamily]
         when 'Debian'
-          it { is_expected.to contain_service('php5-fpm').with_ensure('running') }
-          if facts[:operatingsystem] == 'Ubuntu'
-            if facts[:operatingsystemrelease] == '12.04'
-              it { is_expected.to contain_service('php5-fpm').without_restart }
-            else
-              it { is_expected.to contain_service('php5-fpm').with_restart('service php5-fpm reload') }
-            end
+          case facts[:operatingsystemrelease]
+          when '12.04'
+            it { is_expected.to contain_service('php5-fpm').with_ensure('running').without_restart }
+          when '14.04'
+            it { is_expected.to contain_service('php5-fpm').with_restart('service php5-fpm reload').with_ensure('running') }
+          when '16.04'
+            it { is_expected.to contain_service('php7.0-fpm').with_ensure('running') }
           end
         when 'Suse'
           it { is_expected.to contain_service('php-fpm').with_ensure('running') }

spec/classes/php_fpm_spec.rb
@@ -12,14 +12,18 @@ describe 'php::fpm', type: :class do
         # rubocop:disable RSpec/RepeatedExample
         case facts[:osfamily]
         when 'Debian'
-          it { is_expected.to contain_package('php5-fpm').with_ensure('present') }
-          it { is_expected.to contain_service('php5-fpm').with_ensure('running') }
-          if facts[:operatingsystem] == 'Ubuntu'
-            if facts[:operatingsystemrelease] == '14.04'
-              it { is_expected.to contain_file('/etc/init/php5-fpm.override').with_content('reload signal USR2') }
-            else
-              it { is_expected.to contain_file('/etc/init/php5-fpm.override').with_content("reload signal USR2\nmanual") }
-            end
+          case facts[:operatingsystemrelease]
+          when '14.04'
+            it { is_expected.to contain_file('/etc/init/php5-fpm.override').with_content('reload signal USR2') }
+            it { is_expected.to contain_package('php5-fpm').with_ensure('present') }
+            it { is_expected.to contain_service('php5-fpm').with_ensure('running') }
+          when '12.02'
+            it { is_expected.to contain_file('/etc/init/php5-fpm.override').with_content("reload signal USR2\nmanual") }
+            it { is_expected.to contain_package('php5-fpm').with_ensure('present') }
+            it { is_expected.to contain_service('php5-fpm').with_ensure('running') }
+          when '16.04'
+            it { is_expected.to contain_package('php7.0-fpm').with_ensure('present') }
+            it { is_expected.to contain_service('php7.0-fpm').with_ensure('running') }
           end
         when 'Suse'
           it { is_expected.to contain_package('php5-fpm').with_ensure('present') }

spec/classes/php_spec.rb
@@ -12,11 +12,18 @@ describe 'php', type: :class do
         when 'Debian'
           it { is_expected.not_to contain_class('php::global') }
           it { is_expected.to contain_class('php::fpm') }
-          it { is_expected.to contain_package('php5-cli').with_ensure('present') }
-          it { is_expected.to contain_package('php5-fpm').with_ensure('present') }
-          it { is_expected.to contain_package('php5-dev').with_ensure('present') }
           it { is_expected.to contain_package('php-pear').with_ensure('present') }
           it { is_expected.to contain_class('php::composer') }
+          case facts[:os]['release']['major']
+          when '14.04'
+            it { is_expected.to contain_package('php5-cli').with_ensure('present') }
+            it { is_expected.to contain_package('php5-fpm').with_ensure('present') }
+            it { is_expected.to contain_package('php5-dev').with_ensure('present') }
+          when '16.04'
+            it { is_expected.to contain_package('php7.0-cli').with_ensure('present') }
+            it { is_expected.to contain_package('php7.0-fpm').with_ensure('present') }
+            it { is_expected.to contain_package('php7.0-dev').with_ensure('present') }
+          end
         when 'Suse'
           it { is_expected.to contain_class('php::global') }
           it { is_expected.to contain_package('php5').with_ensure('present') }
@@ -59,7 +66,12 @@ describe 'php', type: :class do
 
         dstfile = case facts[:osfamily]
                   when 'Debian'
-                    '/etc/php5/fpm/pool.d/www.conf'
+                    case facts[:os]['release']['major']
+                    when '16.04'
+                      '/etc/php/7.0/fpm/pool.d/www.conf'
+                    else
+                      '/etc/php5/fpm/pool.d/www.conf'
+                    end
                   when 'Archlinux'
                     '/etc/php/php-fpm.d/www.conf'
                   when 'Suse'
@@ -79,7 +91,12 @@ describe 'php', type: :class do
         it { is_expected.to contain_php__fpm__pool('www').with(group: 'nginx') }
         dstfile = case facts[:osfamily]
                   when 'Debian'
-                    '/etc/php5/fpm/pool.d/www.conf'
+                    case facts[:os]['release']['major']
+                    when '16.04'
+                      '/etc/php/7.0/fpm/pool.d/www.conf'
+                    else
+                      '/etc/php5/fpm/pool.d/www.conf'
+                    end
                   when 'Archlinux'
                     '/etc/php/php-fpm.d/www.conf'
                   when 'Suse'

spec/defines/extension_spec.rb
@@ -12,7 +12,12 @@ describe 'php::extension' do
       unless facts[:osfamily] == 'Suse' || facts[:osfamily] == 'FreeBSD' # FIXME: something is wrong on these
         etcdir = case facts[:osfamily]
                  when 'Debian'
-                   '/etc/php5/mods-available'
+                   case facts[:os]['release']['major']
+                   when '16.04'
+                     '/etc/php/7.0/mods-available'
+                   else
+                     '/etc/php5/mods-available'
+                   end
                  when 'Archlinux'
                    '/etc/php/conf.d'
                  else

spec/defines/fpm_pool_spec.rb
@@ -16,7 +16,12 @@ describe 'php::fpm::pool' do
             let(:title) { 'unique-name' }
             let(:params) { {} }
 
-            it { is_expected.to contain_file('/etc/php5/fpm/pool.d/unique-name.conf') }
+            case facts[:os]['release']['major']
+            when '14.04'
+              it { is_expected.to contain_file('/etc/php5/fpm/pool.d/unique-name.conf') }
+            when '16.04'
+              it { is_expected.to contain_file('/etc/php/7.0/fpm/pool.d/unique-name.conf') }
+            end
           end
         when 'Debian'
           context 'plain config' do

commit a07679baf4b3a4d9654953770c2eedcaae1d4932

use correct require arguments
this fixes an error that was introduced in 0ad75666619af1e3b0b5723a93b5bf9acacf846d

manifests/dev.pp
@@ -40,15 +40,9 @@ class php::dev(
       ensure  => present,
       require => $require,
     })
-
-    package { $real_package:
-      ensure  => $ensure,
-      require => $require,
-    }
-  } else {
-    package { $real_package:
-      ensure  => $ensure,
-      require => $require,
-    }
+  }
+  package { $real_package:
+    ensure  => $ensure,
+    require => Class['::php::packages'],
   }
 }