4544b3bacde497fb8c0ee70b4d58f27989e4f5cf
Tim Meusel <tim@bastelfreak.de>
2018-02-28 00:35:13 +0100

    restructure php56 acceptance tests

--- spec/acceptance/php56_spec.rb
+++ spec/acceptance/php56_spec.rb
@@ -1,6 +1,16 @@
 require 'spec_helper_acceptance'
 
 describe 'with specific php version' do
+  case default[:platform]
+  when %r{ubuntu}
+    packagename = 'php5.6-fpm'
+  when %r{el}
+    # ell = Enterprise Linux = CentOS....
+    packagename = 'php-fpm'
+  when %r{debian}
+    packagename = 'php5-fpm'
+  end
+
   context 'with params' do
     it 'works with 5.6' do
       pp = <<-EOS
@@ -17,20 +27,21 @@ describe 'with specific php version' do
         phpunit      => false,
       }
       EOS
+
       # Run it twice and test for idempotency
       apply_manifest(pp, catch_failures: true)
       apply_manifest(pp, catch_changes: true)
     end
 
-    case default[:platform]
-    when %r{16.04}
-      describe package('php5.6-fpm') do
-        it { is_expected.to be_installed }
-      end
-    when %r{14.04}
-      describe package('php5.6-fpm') do
-        it { is_expected.to be_installed }
-      end
+    describe package(packagename) do
+      it { is_expected.to be_installed }
+    end
+    describe service(packagename) do
+      it { is_expected.to be_running }
+      it { is_expected.to be_enabled }
+    end
+    describe command('php --version') do
+      its(:stdout) { is_expected.to match %r{5\.6} }
     end
   end
 end
