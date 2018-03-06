2b970d75d7b92943e7cc07310315d87beb0f23c7
Tim Meusel <tim@bastelfreak.de>
2018-03-06 14:12:29 +0100

    cleanup php7 acceptance test

--- spec/acceptance/php_spec.rb
+++ spec/acceptance/php_spec.rb
@@ -10,18 +10,22 @@ describe 'php with default settings' do
     end
 
     case default[:platform]
-    when %r{16.04}
-      describe package('php7.0-fpm') do
-        it { is_expected.to be_installed }
-      end
-    when %r{14.04}
-      describe package('php5-fpm') do
-        it { is_expected.to be_installed }
-      end
-    when %(7)
-      describe package('php-fpm') do
-        it { is_expected.to be_installed }
-      end
+    when %r{ubuntu-16.04}
+      packagename = 'php7.0-fpm'
+    when %r{ubuntu-14.04}
+      packagename = 'php5-fpm'
+    when %r{el}
+      packagename = 'php-fpm'
+    when %r{debian}
+      packagename = 'php5-fpm'
+    end
+    describe package(packagename) do
+      it { is_expected.to be_installed }
+    end
+
+    describe service(packagename) do
+      it { is_expected.to be_running }
+      it { is_expected.to be_enabled }
     end
   end
 end
