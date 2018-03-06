b9f81d716d3183b33cbcbaa80f5fb5413259aca7
Michael Kluge <michael@kluge.ws>
2018-03-06 18:41:13 +0100

    PR#292 Compatibility with Softwarecollections (SCL)
    This adds RedHat SCL compatibility to this puppet module
    by adding the rhscl_mode parameter to ::php::globals. By
    specifying this parameter the module itself configures
    path specifications on its own based on the php_version
    string. Further the ::php::extensions define was extended
    by the ability to prefix configs and stack up extensions
    which are merged into one os package.

--- manifests/extension.pp
+++ manifests/extension.pp
@@ -41,7 +41,15 @@
 #  Defaults to false.
 #
 # [*settings*]
-#   Nested hash of global config parameters for php.ini
+#   Hash of parameters for the specific extension, which will be written to the extensions config file by
+#   php::extension::config or a hash of mutliple settings files, each with parameters
+#   (multifile_settings must be true)
+#   (f.ex. {p => '..'} or {'bz2' => {..}, {'math' => {...}})
+#
+# [*multifile_settings*]
+#   Set this to true if you specify multiple setting files in *settings*. This must be used when the PHP package
+#   distribution bundles extensions in a single package (like 'common' bundles extensions 'bz2', ...) and each of
+#   the extension comes with a separate settings file.
 #
 # [*settings_prefix*]
 #   Boolean/String parameter, whether to prefix all setting keys with
@@ -62,12 +70,13 @@ define php::extension (
   String           $ensure                          = 'installed',
   Optional[Php::Provider] $provider                 = undef,
   Optional[String] $source                          = undef,
-  Optional[String] $so_name                         = downcase($name),
+  Optional[String] $so_name                         = undef,
   Optional[String] $ini_prefix                      = undef,
   Optional[String] $php_api_version                 = undef,
   String           $package_prefix                  = $php::package_prefix,
   Boolean          $zend                            = false,
-  Hash             $settings                        = {},
+  Variant[Hash, Hash[String, Hash]] $settings       = {},
+  Boolean          $multifile_settings              = false,
   Php::Sapi        $sapi                            = 'ALL',
   Variant[Boolean, String]       $settings_prefix   = false,
   Optional[Stdlib::AbsolutePath] $responsefile      = undef,
@@ -93,17 +102,29 @@ define php::extension (
 
   # PEAR packages don't require any further configuration, they just need to "be there".
   if $provider != 'pear' {
-    php::extension::config { $title:
-      ensure          => $ensure,
-      provider        => $provider,
-      so_name         => $so_name,
-      ini_prefix      => $ini_prefix,
-      php_api_version => $php_api_version,
-      zend            => $zend,
-      settings        => $settings,
-      settings_prefix => $settings_prefix,
-      sapi            => $sapi,
-      subscribe       => Php::Extension::Install[$title],
+    $_settings = $multifile_settings ? {
+      true  => $settings,
+      false => { downcase($title) => $settings } # emulate a hash if no multifile settings
+    }
+
+    $_settings.each |$settings_name, $settings_hash| {
+      $so_name = $multifile_settings ? {
+        true  => downcase($settings_name),
+        false => pick(downcase($so_name), downcase($name), downcase($settings_name)),
+      }
+
+      php::extension::config { $settings_name:
+        ensure          => $ensure,
+        provider        => $provider,
+        so_name         => $so_name,
+        ini_prefix      => $ini_prefix,
+        php_api_version => $php_api_version,
+        zend            => $zend,
+        settings        => $settings_hash,
+        settings_prefix => $settings_prefix,
+        sapi            => $sapi,
+        subscribe       => Php::Extension::Install[$title],
+      }
     }
   }
 }
--- spec/classes/php_spec.rb
+++ spec/classes/php_spec.rb
@@ -64,6 +64,11 @@ describe 'php', type: :class do
 
       describe 'when called with no parameters' do
         case facts[:osfamily]
+        when 'Suse', 'RedHat', 'CentOS'
+          it { is_expected.to contain_class('php::global') }
+        end
+
+        case facts[:osfamily]
         when 'Debian'
           it { is_expected.not_to contain_class('php::global') }
           it { is_expected.to contain_class('php::fpm') }
@@ -73,36 +78,49 @@ describe 'php', type: :class do
           it { is_expected.to contain_package(php_fpm_ackage).with_ensure('present') }
           it { is_expected.to contain_package(php_dev_ackage).with_ensure('present') }
         when 'Suse'
-          it { is_expected.to contain_class('php::global') }
           it { is_expected.to contain_package('php5').with_ensure('present') }
           it { is_expected.to contain_package('php5-devel').with_ensure('present') }
           it { is_expected.to contain_package('php5-pear').with_ensure('present') }
           it { is_expected.not_to contain_package('php5-cli') }
           it { is_expected.not_to contain_package('php5-dev') }
           it { is_expected.not_to contain_package('php-pear') }
+        when 'RedHat', 'CentOS'
+          it { is_expected.to contain_package('php-cli').with_ensure('present') }
+          it { is_expected.to contain_package('php-common').with_ensure('present') }
         end
       end
 
       describe 'when called with package_prefix parameter' do
-        let(:params) { { package_prefix: 'myphp-' } }
+        package_prefix = 'myphp-'
+        let(:params) { { package_prefix: package_prefix } }
+
+        case facts[:osfamily]
+        when 'Suse', 'RedHat', 'CentOS'
+          it { is_expected.to contain_class('php::global') }
+        end
+
+        case facts[:osfamily]
+        when 'Debian', 'RedHat', 'CentOS'
+          it { is_expected.to contain_package("#{package_prefix}cli").with_ensure('present') }
+        end
 
         case facts[:osfamily]
         when 'Debian'
           it { is_expected.not_to contain_class('php::global') }
           it { is_expected.to contain_class('php::fpm') }
-          it { is_expected.to contain_package('myphp-cli').with_ensure('present') }
-          it { is_expected.to contain_package('myphp-fpm').with_ensure('present') }
-          it { is_expected.to contain_package('myphp-dev').with_ensure('present') }
-          it { is_expected.to contain_package('php-pear').with_ensure('present') }
           it { is_expected.to contain_class('php::composer') }
+          it { is_expected.to contain_package('php-pear').with_ensure('present') }
+          it { is_expected.to contain_package("#{package_prefix}dev").with_ensure('present') }
+          it { is_expected.to contain_package("#{package_prefix}fpm").with_ensure('present') }
         when 'Suse'
-          it { is_expected.to contain_class('php::global') }
           it { is_expected.to contain_package('php5').with_ensure('present') }
-          it { is_expected.to contain_package('myphp-devel').with_ensure('present') }
-          it { is_expected.to contain_package('myphp-pear').with_ensure('present') }
-          it { is_expected.not_to contain_package('myphp-cli') }
-          it { is_expected.not_to contain_package('myphp-dev') }
+          it { is_expected.to contain_package("#{package_prefix}devel").with_ensure('present') }
+          it { is_expected.to contain_package("#{package_prefix}pear").with_ensure('present') }
+          it { is_expected.not_to contain_package("#{package_prefix}cli").with_ensure('present') }
+          it { is_expected.not_to contain_package("#{package_prefix}dev") }
           it { is_expected.not_to contain_package('php-pear') }
+        when 'RedHat', 'CentOS'
+          it { is_expected.to contain_package("#{package_prefix}common").with_ensure('present') }
         end
       end
 
@@ -188,10 +206,59 @@ describe 'php', type: :class do
 
         it { is_expected.not_to contain_class('php::fpm') }
       end
+
       describe 'when composer is disabled' do
         let(:params) { { composer: false } }
 
         it { is_expected.not_to contain_class('php::composer') }
+      end
+
+      if facts[:osfamily] == 'RedHat' || facts[:osfamily] == 'CentOS'
+        describe 'when called with global option for rhscl_mode' do
+          describe 'when called with mode "remi"' do
+            scl_php_version = 'php56'
+            rhscl_mode = 'remi'
+            let(:pre_condition) do
+              "class {'::php::globals':
+                        php_version => '#{scl_php_version}',
+                        rhscl_mode => '#{rhscl_mode}'
+              }"
+            end
+            let(:params) do
+              { settings: { 'Date/date.timezone' => 'Europe/Berlin' } }
+            end
+
+            it { is_expected.to contain_class('php::global') }
+            it { is_expected.to contain_package("#{scl_php_version}-php-cli").with_ensure('present') }
+            it { is_expected.to contain_package("#{scl_php_version}-php-common").with_ensure('present') }
+            it { is_expected.to contain_php__config('global').with(file: "/etc/opt/#{rhscl_mode}/#{scl_php_version}/php.ini") }
+            it { is_expected.not_to contain_php__config('cli') }
+
+            # see: https://github.com/voxpupuli/puppet-php/blob/master/lib/puppet/parser/functions/to_hash_settings.rb
+            it { is_expected.to contain_php__config__setting("/etc/opt/#{rhscl_mode}/#{scl_php_version}/php.ini: Date/date.timezone").with_value('Europe/Berlin') }
+          end
+
+          describe 'when called with mode "rhscl"' do
+            scl_php_version = 'rh-php56'
+            rhscl_mode = 'rhscl'
+            let(:pre_condition) do
+              "class {'::php::globals':
+                        php_version => '#{scl_php_version}',
+                        rhscl_mode => '#{rhscl_mode}'
+              }"
+            end
+            let(:params) do
+              { settings: { 'Date/date.timezone' => 'Europe/Berlin' } }
+            end
+
+            it { is_expected.to contain_class('php::global') }
+            it { is_expected.to contain_package("#{scl_php_version}-php-cli").with_ensure('present') }
+            it { is_expected.to contain_package("#{scl_php_version}-php-common").with_ensure('present') }
+            it { is_expected.to contain_php__config('global').with(file: "/etc/opt/rh/#{scl_php_version}/php.ini") }
+            it { is_expected.to contain_php__config('cli').with(file: "/etc/opt/rh/#{scl_php_version}/php-cli.ini") }
+            it { is_expected.to contain_php__config__setting("/etc/opt/rh/#{scl_php_version}/php.ini: Date/date.timezone").with_value('Europe/Berlin') }
+          end
+        end
       end
     end
   end
