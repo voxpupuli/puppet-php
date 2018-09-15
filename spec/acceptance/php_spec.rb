aaeb55552af43e95f5e21dadb132f89a96484f28
Tim Meusel <tim@bastelfreak.de>
2018-09-15 11:34:32 +0200

    Add acceptance tests for system php with extensions

--- spec/acceptance/php_spec.rb
+++ spec/acceptance/php_spec.rb
@@ -32,4 +32,42 @@ describe 'php with default settings' do
       it { is_expected.to be_enabled }
     end
   end
+  context 'default parameters with extensions' do
+    it 'works with defaults' do
+      pp = <<-EOS
+      class{'php':
+        extensions => {
+        'mysql'    => {},
+        'gd'       => {}
+        }
+      }
+      EOS
+      # Run it twice and test for idempotency
+      apply_manifest(pp, catch_failures: true)
+      apply_manifest(pp, catch_changes: true)
+    end
+
+    case default[:platform]
+    when %r{ubuntu-18.04}
+      packagename = 'php7.2-fpm'
+    when %r{ubuntu-16.04}
+      packagename = 'php7.0-fpm'
+    when %r{ubuntu-14.04}
+      packagename = 'php5-fpm'
+    when %r{el}
+      packagename = 'php-fpm'
+    when %r{debian-8}
+      packagename = 'php5-fpm'
+    when %r{debian-9}
+      packagename = 'php7.0-fpm'
+    end
+    describe package(packagename) do
+      it { is_expected.to be_installed }
+    end
+
+    describe service(packagename) do
+      it { is_expected.to be_running }
+      it { is_expected.to be_enabled }
+    end
+  end
 end
