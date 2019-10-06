c6e62b54c1187cabb846c0397756eba934f0cc9f
Matthieu FAURE <mfaure@asqatasun.org>
2019-10-06 11:12:42 +0200

    CHORE Acceptance: add php extension xmlreader

--- spec/acceptance/php_spec.rb
+++ spec/acceptance/php_spec.rb
@@ -44,6 +44,7 @@ describe 'php class' do
                           'intl'      => {},
                           'json'      => {},
                           'mbstring'  => {},
+                          'xmlreader' => {},
                           'zip'       => {},
                           'net-url'  => {
                             package_prefix => 'php-',
