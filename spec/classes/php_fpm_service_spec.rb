b0b484133d31022b31980421c73d4257f9e8822e
Tim Meusel <tim@bastelfreak.de>
2020-08-01 14:09:30 +0200

    increase spec coverage

--- spec/classes/php_fpm_service_spec.rb
+++ spec/classes/php_fpm_service_spec.rb
@@ -9,6 +9,20 @@ describe 'php::fpm::service', type: :class do
 
       let(:pre_condition) { 'class {"php": fpm => true}' }
 
+      describe 'works without default' do
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_class('php::cli') }
+        it { is_expected.to contain_class('php::composer::auto_update') }
+        it { is_expected.to contain_class('php::composer') }
+        it { is_expected.to contain_class('php::dev') }
+        it { is_expected.to contain_class('php::fpm::config') }
+        it { is_expected.to contain_class('php::fpm') }
+        it { is_expected.to contain_class('php::packages') }
+        it { is_expected.to contain_class('php::globals') }
+        it { is_expected.to contain_class('php::params') }
+        it { is_expected.to contain_class('php::pear') }
+      end
+
       describe 'when called with no parameters' do
         # rubocop:disable RSpec/RepeatedExample
         case facts[:osfamily]
--- spec/classes/php_repo_ubuntu_spec.rb
+++ spec/classes/php_repo_ubuntu_spec.rb
@@ -7,42 +7,13 @@ describe 'php::repo::ubuntu', type: :class do
         facts
       end
 
-      case facts[:lsbdistcodename]
-      when 'trusty'
-        describe 'when called with no parameters on Ubuntu trusty' do
-          it { is_expected.to contain_exec('add-apt-repository-ppa:ondrej/php') }
+      describe 'works without params' do
+        if facts[:os]['name'] == 'Ubuntu'
+          it { is_expected.to compile.with_all_deps }
+          it { is_expected.to contain_apt__ppa('ppa:ondrej/php') }
+        else
+          it { is_expected.to compile.and_raise_error(%r{class php::repo::ubuntu does not work on OS}) }
         end
-
-        describe 'when called with version 7.0 on Ubuntu trusty' do
-          let(:params) do
-            {
-              version: '7.0'
-            }
-          end
-
-          it { is_expected.to contain_exec('add-apt-repository-ppa:ondrej/php') }
-        end
-
-        describe 'when call with version 5.6 on Ubuntu trusty' do
-          let(:params) do
-            {
-              version: '5.6'
-            }
-          end
-
-          it { is_expected.to contain_exec('add-apt-repository-ppa:ondrej/php') }
-        end
-
-        describe 'when call with version 5.4 on Ubuntu trusty' do
-          let(:params) do
-            {
-              version: '5.4'
-            }
-          end
-
-          it { is_expected.to raise_error(Puppet::Error) }
-        end
-
       end
     end
   end
